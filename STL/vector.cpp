d7095d869350aba87adb28444cd35136f784d17c
e11438 <isankalpa7@gmail.com>
2021-10-27 17:59:30 +0530

    added emplace_back

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -1,11 +1,38 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
+class Check{
+    public:
+        float x;
+
+        Check(float in):x(in)   //parameterized constructor
+        {
+
+        }
+
+        Check( const Check & ch):x(ch.x)    //copy constructor
+        {
+            cout << "coping Check.." << endl;
+        }
+};
 
 int main()
 {
-    cout << "vector checks" << endl;
+
+    vector<Check> ch_vec;
+
+    ch_vec.reserve(2);
+
+    ch_vec.push_back(Check(1));
+
+    ch_vec.emplace_back(2);
+    cout << "Done intserting "<< endl;
+    for(Check ch : ch_vec)
+    {
+        cout << ch.x << endl;
+    }  
 
     return 0;
 }
